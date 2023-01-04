7c9d80b46f2400a9bdc6c30055189aedd8247bd0
maojunfeng <1009935357@qq.com>
2023-01-04 16:29:15 +0800

    feature-test logic

--- lib/pages/test/test_logic.dart
+++ lib/pages/test/test_logic.dart
@@ -1,5 +1,13 @@
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TestLogic extends GetxController {
   var count = 0.obs;
+
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isFirstOpen', true);
+  }
 }
