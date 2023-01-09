d49f93d1d98c1cf2f0695818aa5fef93d0118ea0
maojunfeng <1009935357@qq.com>
2023-01-09 15:59:20 +0800

    feature-guide page

--- lib/pages/guide/guide_logic.dart
+++ lib/pages/guide/guide_logic.dart
@@ -1,11 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class GuideLogic extends GetxController {
+import '../../core/config.dart';
+
+class GuideLogic extends GetxController with GetTickerProviderStateMixin {
+  var currentPageIndex = 0.obs;
+  var pageController = PageController();
+  var guideIllustrations = AppConfig.guideIllustrations;
+  late List<AnimationController> animationControllerList = guideIllustrations
+      .map((illustration) => AnimationController(vsync: this))
+      .toList();
+
   @override
-  Future<void> onInit() async {
+  void onInit() {
     super.onInit();
+    pageController.addListener(() {
+      var page = pageController.page ?? 0;
+      animationControllerList[page.toInt()].value = 1 - (page - page.toInt());
+      if (animationControllerList.length > page.toInt() + 1) {
+        animationControllerList[page.toInt() + 1].value = page - page.toInt();
+      }
+    });
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    if (animationControllerList.isNotEmpty) {
+      animationControllerList[0]
+        ..duration = const Duration(seconds: 3)
+        ..forward();
+    }
+  }
+
+  void onButtonPressed() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isFirstOpen', false);
+    Get.toNamed(RouteGet.testPage);
   }
 }
--- lib/pages/guide/guide_view.dart
+++ lib/pages/guide/guide_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
-import '../../core/config.dart';
 import 'guide_logic.dart';
 
 class GuidePage extends StatelessWidget {
@@ -13,15 +12,64 @@ class GuidePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          PageView(
-            children: AppConfig.guideIllustrations
-                .map((illustration) => Lottie.asset(illustration))
-                .toList(),
+      backgroundColor: Colors.white,
+      body: Stack(children: [
+        PageView(
+          onPageChanged: (index) => logic.currentPageIndex.value = index,
+          controller: logic.pageController,
+          children: logic.guideIllustrations
+              .asMap()
+              .entries
+              .map(
+                (entry) => Lottie.asset(entry.value,
+                    controller: logic.animationControllerList[entry.key]),
+              )
+              .toList(),
+        ),
+        Column(children: [
+          Flexible(flex: 17, child: Container()),
+          Flexible(
+            flex: 3,
+            child: Obx(() {
+              return logic.currentPageIndex.value ==
+                      logic.guideIllustrations.length - 1
+                  ? TextButton(
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.resolveWith(
+                            (states) => const EdgeInsets.all(16)),
+                        backgroundColor: MaterialStateProperty.resolveWith(
+                            (states) => Colors.green.withOpacity(0.2)),
+                      ),
+                      onPressed: logic.onButtonPressed,
+                      child: const Text(
+                        '开启笔记',
+                        style: TextStyle(fontSize: 26, color: Colors.green),
+                      ),
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: logic.guideIllustrations
+                          .asMap()
+                          .keys
+                          .map((index) => Obx(() {
+                                return Container(
+                                  width: 8,
+                                  height: 8,
+                                  margin:
+                                      const EdgeInsets.symmetric(horizontal: 4),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: logic.currentPageIndex.value == index
+                                        ? Colors.grey
+                                        : Colors.grey.withOpacity(0.3),
+                                  ),
+                                );
+                              }))
+                          .toList());
+            }),
           )
-        ],
-      ),
+        ]),
+      ]),
     );
   }
 }
