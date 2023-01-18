709e23b57a2fa74e3f7c047c3cb3e975dd64325e
maojunfeng <1009935357@qq.com>
2023-01-18 18:24:56 +0800

    feature-setting page

--- lib/pages/settings/settings_view.dart
+++ lib/pages/settings/settings_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:object_note/widgets/base_app_bar.dart';
 
+import '../../core/app_controller.dart';
 import 'settings_logic.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -9,22 +10,67 @@ class SettingsPage extends StatelessWidget {
 
   final logic = Get.put(SettingsLogic());
   final state = Get.find<SettingsLogic>().state;
+  final AppController appController = Get.find();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: TextButton(
-          onPressed: () {
-            showCupertinoModalBottomSheet(
-                context: context,
-                backgroundColor: Colors.orange,
-                builder: (context) {
-                  return Container();
-                });
-          },
-          child: const Text('modal bottom sheet'),
+      appBar: createBaseAppBar(),
+      body: Card(
+        margin: const EdgeInsets.all(20),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Theme',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 10),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  //color: Theme.of(context).colorScheme.surface,
+                  child: Column(
+                    children: [
+                      RadioListTile(
+                        activeColor: Theme.of(context).colorScheme.primary,
+                        title: const Text('system'),
+                        value: ThemeMode.system,
+                        groupValue: appController.themeMode.value,
+                        onChanged: (value) {
+                          appController.setThemeMode(ThemeMode.system);
+                        },
+                      ),
+                      RadioListTile(
+                        activeColor: Theme.of(context).colorScheme.primary,
+                        title: const Text('dark'),
+                        value: ThemeMode.dark,
+                        groupValue: appController.themeMode.value,
+                        onChanged: (value) {
+                          appController.setThemeMode(ThemeMode.dark);
+                        },
+                      ),
+                      RadioListTile(
+                        activeColor: Theme.of(context).colorScheme.primary,
+                        title: const Text('light'),
+                        value: ThemeMode.light,
+                        groupValue: appController.themeMode.value,
+                        onChanged: (value) async {
+                          appController.setThemeMode(ThemeMode.light);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('是大家纷纷开始搭建')),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
