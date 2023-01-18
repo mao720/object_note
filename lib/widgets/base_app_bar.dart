326582d5410ad638db66d5eb9361545af3b75f10
maojunfeng <1009935357@qq.com>
2023-01-18 18:24:45 +0800

    feature-base app bar

---
+++ lib/widgets/base_app_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+PreferredSizeWidget createBaseAppBar({
+  String title = 'AppBar',
+  bool showLeading = true,
+  Color? color,
+  Function()? onMenuPressed,
+}) {
+  List<Widget> actions = [];
+  if (onMenuPressed != null) {
+    actions.add(
+      SizedBox(
+        width: kToolbarHeight,
+        child: IconButton(
+          onPressed: onMenuPressed,
+          tooltip: 'menu',
+          color: color,
+          icon: const Icon(Icons.menu),
+        ),
+      ),
+    );
+  }
+  return AppBar(
+    title: Text(title, style: TextStyle(color: color)),
+    centerTitle: true,
+    leading: showLeading
+        ? IconButton(
+            onPressed: () => Get.back(),
+            tooltip: 'Back',
+            color: color,
+            icon: const Icon(Icons.arrow_back),
+          )
+        : null,
+    actions: actions,
+  );
+}
