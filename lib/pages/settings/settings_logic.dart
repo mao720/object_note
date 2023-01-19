960d73f973f9e5ddbd251b672b69bad00298a5a9
maojunfeng <1009935357@qq.com>
2023-01-19 11:13:40 +0800

    feature-setting logic

--- lib/pages/settings/settings_logic.dart
+++ lib/pages/settings/settings_logic.dart
@@ -1,7 +1,9 @@
 import 'package:get/get.dart';
 
+import '../../core/app_controller.dart';
 import 'settings_state.dart';
 
 class SettingsLogic extends GetxController {
   final SettingsState state = SettingsState();
+  final AppController appController = Get.find();
 }
