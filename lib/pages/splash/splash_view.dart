a28129a0be98264da4bba3e09d519e580afbde41
maojunfeng <1009935357@qq.com>
2023-02-03 18:16:29 +0800

    feature-splash page

--- lib/pages/splash/splash_view.dart
+++ lib/pages/splash/splash_view.dart
@@ -13,7 +13,6 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
