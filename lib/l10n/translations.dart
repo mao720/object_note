39e2d4309323f907981918338ef5b362f996f2cc
maojunfeng <1009935357@qq.com>
2023-02-06 14:03:03 +0800

    feature-translations

--- lib/l10n/translations.dart
+++ lib/l10n/translations.dart
@@ -13,39 +13,43 @@ class TranslationStrings extends Translations {
 
   @override
   Map<String, Map<String, String>> get keys => {
-        'zh_Hans': {
-          //Home page Tabs
-          'Home': '主页',
-          'Mine': '我的',
-          //Mine Tab
-          'Splash Page': '启动页',
-          'Guide Page': '导航页',
-          //Guide page
-          'Start Note': '开启笔记',
-          //Settings page
-          'Settings': '设置',
-          'Theme': '主题',
-          'System': '系统',
-          'Light': '浅色',
-          'Dark': '深色',
-          'Language': '语言',
-        },
-        'en': {
-          //Home page Tabs
-          'Home': 'Home',
-          'Mine': 'Mine',
-          //Mine tab
-          'Splash Page': 'Splash Page',
-          'Guide Page': 'Guide Page',
-          //Guide page
-          'Start Note': 'Start Note',
-          //Settings page
-          'Settings': 'Settings',
-          'Theme': 'Theme',
-          'System': 'System',
-          'Light': 'Light',
-          'Dark': 'Dark',
-          'Language': 'Language',
-        },
+        'zh_Hans': zhHans,
+        'en': en,
       };
+
+  final Map<String, String> zhHans = {
+    //Home page Tabs
+    'Home': '主页',
+    'Mine': '我的',
+    //Mine Tab
+    'Splash Page': '启动页',
+    'Guide Page': '导航页',
+    //Guide page
+    'Start Note': '开启笔记',
+    //Settings page
+    'Settings': '设置',
+    'Theme': '主题',
+    'System': '系统',
+    'Light': '浅色',
+    'Dark': '深色',
+    'Language': '语言',
+  };
+
+  final Map<String, String> en = {
+    //Home page Tabs
+    'Home': 'Home',
+    'Mine': 'Mine',
+    //Mine tab
+    'Splash Page': 'Splash Page',
+    'Guide Page': 'Guide Page',
+    //Guide page
+    'Start Note': 'Start Note',
+    //Settings page
+    'Settings': 'Settings',
+    'Theme': 'Theme',
+    'System': 'System',
+    'Light': 'Light',
+    'Dark': 'Dark',
+    'Language': 'Language',
+  };
 }
