56e5bb797e0453cf4c915942bc69cdf8bb84a152
maojunfeng <1009935357@qq.com>
2023-02-07 20:25:20 +0800

    feature-assets

---
+++ lib/core/assets.dart
@@ -0,0 +1,17 @@
+class Assets {
+  static const splashLogo = 'assets/animations/logo-somar.json';
+  static const yogaBoxBreathing = 'assets/animations/yoga-box-breathing.json';
+
+  static const illustrationStayAtHome =
+      'assets/animations/illustration-stay-at-home.json';
+  static const illustrationBook = 'assets/animations/illustration-book.json';
+  static const illustrationBookshelf =
+      'assets/animations/illustration-bookshelf.json';
+  static const illustrationHeart = 'assets/animations/illustration-heart.json';
+  static const illustrationPlantingTrees =
+      'assets/animations/illustration-planting-trees.json';
+  static const illustrationWalking =
+      'assets/animations/illustration-walking.json';
+  static const illustrationWateringFlowers =
+      'assets/animations/illustration-watering-flowers.json';
+}
