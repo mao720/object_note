7e45c828862c450248e16d13da6d891c2dba0068
maojunfeng <1009935357@qq.com>
2023-02-11 19:10:55 +0800

    feature-settings

--- lib/pages/settings/settings_logic.dart
+++ lib/pages/settings/settings_logic.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:object_note/core/app_config.dart';
+import 'package:object_note/core/global.dart';
 import 'package:object_note/pages/settings/settings_state.dart';
 
 class SettingsLogic extends GetxController {
   final SettingsState state = SettingsState();
 
   onThemeChanged(ThemeMode themeMode) {
-    AppConfig.setThemeMode(themeMode);
+    Global.setThemeMode(themeMode);
   }
 
   onLocaleChanged(String locale) {
-    AppConfig.setLocale(locale);
+    Global.setLocale(locale);
   }
 }
