a631de9475f97711d3ddbd7d1b54b4ad68344ae3
maojunfeng <1009935357@qq.com>
2023-02-11 19:10:23 +0800

    feature-guide logic

--- lib/pages/guide/guide_logic.dart
+++ lib/pages/guide/guide_logic.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:object_note/core/http.dart';
 import 'package:object_note/pages/guide/guide_state.dart';
+import 'package:object_note/utils/log_util.dart';
+import 'package:object_note/widgets/toast.dart';
 
 class GuideLogic extends GetxController with GetTickerProviderStateMixin {
   final GuideState state = GuideState();
@@ -41,6 +44,13 @@ class GuideLogic extends GetxController with GetTickerProviderStateMixin {
   }
 
   void onButtonPressed() async {
+    Http().get('classes/_User').then((value) => Log.d(value.toString()));
+    if (Toast.isShow) {
+      Toast.dismiss();
+      // Toast.progress(0.6,text: 'onButton');
+    } else {
+      Toast.progress(0.6, text: 'onButton');
+    }
     // SharedPreferences prefs = await SharedPreferences.getInstance();
     // await prefs.setBool('isFirstOpen', false);
     // Get.offNamed(RouteConfig.homePage);
