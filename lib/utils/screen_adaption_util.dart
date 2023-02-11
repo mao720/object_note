c11398fe67fa503e5fb53f3c3f825c9e34a92648
maojunfeng <1009935357@qq.com>
2023-02-11 19:10:48 +0800

    feature-screen

--- lib/utils/screen_adaption_util.dart
+++ lib/utils/screen_adaption_util.dart
@@ -1,34 +1,32 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:object_note/core/global.dart';
+import 'package:object_note/core/styles.dart';
 import 'package:object_note/utils/log_util.dart';
 
 class ScreenAdaptionUtil {
-  static const Size _designSize = Size(360, 690);
-  static double _scaleWidth = 1.0;
-  static double _scaleHeight = 1.0;
+  static final ScreenAdaptionUtil _instance = ScreenAdaptionUtil._init();
 
-  static ScreenAdaptionUtil? _instance;
+  factory ScreenAdaptionUtil() => _instance;
 
-  factory ScreenAdaptionUtil.init(BuildContext context) {
-    _instance ??= ScreenAdaptionUtil._(context);
-    return _instance!;
+  double _scaleWidth = 1.0;
+  double _scaleHeight = 1.0;
+
+  ScreenAdaptionUtil._init() {
+    Log.d('ScreenAdaptionUtil init');
+    Size size = MediaQuery.of(Global.context).size;
+    _scaleWidth = min(size.width, size.height) / Styles.designSize.width;
+    _scaleHeight = max(size.width, size.height) / Styles.designSize.height;
   }
 
-  ScreenAdaptionUtil._(BuildContext context) {
-    Log.d('ScreenAdaptionUtil init()');
-    Size size = MediaQuery.of(context).size;
-    _scaleWidth = min(size.width, size.height) / _designSize.width;
-    _scaleHeight = max(size.width, size.height) / _designSize.height;
-  }
+  scaleWidth(num a) => a * _scaleWidth;
 
-  static scaleWidth(num a) => a * _scaleWidth;
-
-  static scaleHeight(num a) => a * _scaleHeight;
+  scaleHeight(num a) => a * _scaleHeight;
 }
 
 extension ScreenAdaptionExtension on num {
-  double get w => ScreenAdaptionUtil.scaleWidth(this);
+  double get w => ScreenAdaptionUtil().scaleWidth(this);
 
-  double get h => ScreenAdaptionUtil.scaleHeight(this);
+  double get h => ScreenAdaptionUtil().scaleHeight(this);
 }
