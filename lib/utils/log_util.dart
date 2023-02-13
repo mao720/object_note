1c71831d5bc4cc0e90891a688a1064d1268f91b8
maojunfeng <1009935357@qq.com>
2023-02-13 19:38:52 +0800

    feature-log util

--- lib/utils/log_util.dart
+++ lib/utils/log_util.dart
@@ -5,7 +5,15 @@ import 'package:flutter/foundation.dart';
 class Log {
   Log._();
 
-  static void d(String value, {bool isError = false}) {
-    if (isError || kDebugMode) developer.log(value, name: 'ObjectNote');
+  static void d(Object value, {String? tag}) {
+    if (kDebugMode) {
+      developer.log(value.toString(),
+          name: 'ObjectNote${tag == null ? '' : (':$tag')}');
+    }
+  }
+
+  static void r(Object value, {String? tag}) {
+    // ignore: avoid_print
+    print('[ObjectNote${tag == null ? ']' : (':$tag]')}${value.toString()}');
   }
 }
