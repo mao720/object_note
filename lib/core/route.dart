2dd47c5f6bcbd623e8cba2485be58401baecfd3b
maojunfeng <1009935357@qq.com>
2023-02-14 18:23:13 +0800

    feature-route

--- lib/core/route.dart
+++ lib/core/route.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:object_note/pages/guide/guide_view.dart';
 import 'package:object_note/pages/home/home_view.dart';
+import 'package:object_note/pages/login/login_view.dart';
 import 'package:object_note/pages/settings/settings_view.dart';
 import 'package:object_note/pages/splash/splash_view.dart';
 
@@ -10,7 +11,7 @@ class AppRoute {
   static const String guidePage = "/guidePage";
   static const String homePage = "/homePage";
   static const String settingsPage = "/settingsPage";
-  static const String testPage = "/testPage";
+  static const String loginPage = "/loginPage";
 
   ///pages map
   static final List<GetPage> getPages = [
@@ -18,5 +19,6 @@ class AppRoute {
     GetPage(name: guidePage, page: () => GuidePage()),
     GetPage(name: homePage, page: () => HomePage()),
     GetPage(name: settingsPage, page: () => SettingsPage()),
+    GetPage(name: loginPage, page: () => LoginPage()),
   ];
 }
