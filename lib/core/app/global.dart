02c22bdb4252ac871deedc1b9ac62e1b5c8dbd2b
maojunfeng <1009935357@qq.com>
2023-02-15 18:17:22 +0800

    feature-global

---
+++ lib/core/app/global.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:object_note/core/styles/constants.dart';
+import 'package:object_note/core/styles/styles.dart';
+import 'package:object_note/core/utils/log_util.dart';
+import 'package:object_note/core/utils/string_utils.dart';
+import 'package:object_note/modal/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Global {
+  static final Global _instance = Global._init();
+
+  factory Global.init() => _instance;
+
+  static late BuildContext context;
+  static late SharedPreferences prefs;
+  static Rx<User?> user = Rx(null);
+
+  Global._init() {
+    WidgetsFlutterBinding.ensureInitialized();
+    Log.d('AppConfig init()');
+    SharedPreferences.getInstance().then((sharedPreferences) {
+      prefs = sharedPreferences;
+      //theme
+      String theme = prefs.getString(Constants.theme) ?? 'system';
+      setThemeMode(ThemeMode.values.firstWhere((e) => describeEnum(e) == theme,
+          orElse: () => ThemeMode.system));
+      //locale
+      String locale = prefs.getString(Constants.locale) ?? 'system';
+      setLocale(locale);
+      //User
+      String userJson = prefs.getString(Constants.user) ?? '';
+      if (userJson.isNotEmpty) user.value = User.fromJson(jsonDecode(userJson));
+    });
+  }
+
+  static Future<void> setThemeMode(ThemeMode themeMode) async {
+    Styles.themeMode.value = themeMode;
+    Get.changeThemeMode(themeMode);
+    await prefs.setString(Constants.theme, describeEnum(themeMode));
+  }
+
+  static Future<void> setLocale(String localeString) async {
+    Styles.locale.value = localeString;
+    Get.updateLocale(
+        StringUtils.stringToLocale(localeString, Get.deviceLocale));
+    await prefs.setString(Constants.locale, localeString);
+  }
+
+  static Future<void> setUser(User? user) async {
+    Global.user.value = user;
+    if (user == null) {
+      await prefs.remove(Constants.user);
+    } else {
+      await prefs.setString(Constants.user, jsonEncode(user.toJson()));
+    }
+  }
+}
