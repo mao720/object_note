f083a82ce24a475caaca8c74c50a0cb6e946b5c9
maojunfeng <1009935357@qq.com>
2023-02-15 18:17:04 +0800

    feature-app bar

--- lib/widgets/base_app_bar.dart
+++ lib/widgets/base_app_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:object_note/utils/screen_adaption_util.dart';
+import 'package:object_note/core/utils/screen_adaption_util.dart';
 
 PreferredSizeWidget createBaseAppBar({
   String title = 'AppBar',
