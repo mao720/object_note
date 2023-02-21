778275f3de064d1b26f3e58aa4266a7e8a7eebb6
maojunfeng <1009935357@qq.com>
2023-02-21 14:34:18 +0800

    feature-home page

--- lib/pages/home/home_logic.dart
+++ lib/pages/home/home_logic.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:object_note/core/app/app_route.dart';
 import 'package:object_note/pages/home/home_state.dart';
 
 class HomeLogic extends GetxController {
   final HomeState state = HomeState();
   final PageController pageController = PageController();
-
-  onSettingButtonPressed() async {
-    Get.toNamed(AppRoute.settingsPage);
-  }
-
-  onListTilePressed(String title) {
-    switch (title) {
-      case 'Splash Page':
-        Get.toNamed(AppRoute.splashPage);
-        break;
-      case 'Guide Page':
-        Get.toNamed(AppRoute.guidePage);
-        break;
-    }
-  }
 
   onPageChanged(int index) {
     state.currentIndex.value = index;
@@ -33,9 +17,5 @@ class HomeLogic extends GetxController {
       duration: const Duration(milliseconds: 300),
       curve: Curves.ease,
     );
-  }
-
-  onUserHeaderPressed() {
-    Get.toNamed(AppRoute.profilePage);
   }
 }
